f74627f62d851a0174d425e70db11fab660ca2c1
Krupesh Vithlani <76093323+tkrupesh14@users.noreply.github.com>
2022-03-01 20:12:56 +0530

    Day 1 Answers

---
+++ Questions/Day-1/cpp/Q1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double d = 0;
+    float f = 0;
+    int i = 0;
+    string s = "";
+    char c = '0';
+
+    cin>>d;
+    cin>>f;
+    cin>>i;
+    cin>>s;
+    cin>>c;
+
+    cout<<d<<endl;
+    cout<<f<<endl;
+    cout<<i<<endl;
+    cout<<s<<endl;
+    cout<<c<<endl;
+
+    return 0;
+}
---
+++ Questions/Day-1/cpp/Q2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    cout<<"Enter a: ";
+     cin>>a;
+
+    int b = 0;
+    cout<<"Enter b: ";
+    cin>>b;
+
+    cout<<"\nSum of a and b is "<<(a + b);
+
+    return 0;
+}
